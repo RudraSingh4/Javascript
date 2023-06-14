c43ac2d22f58f66128109f1a63b561cd725583c1
Rudra Singh <rudra.singh@cloudeq.com>
2023-06-14 16:48:18 +0530

    condition

--- condition.js
+++ condition.js
@@ -1,5 +1,69 @@
-let age= 14;
-if (age>=14){
-    alert("You can Vote!");
-    alert("hey")
+// let age= 14;
+// if (age>=14){
+//     alert("You can Vote!");
+//     alert("hey")
+// }
+
+let age1 = 19;
+if(age1>18){
+    console.log("User can play");
+} else {
+    console.log("user can play Mario");
+}
+
+let num = 14;
+if (num%2===0){
+    console.log("even");
+}else{
+    console.log("Odd");
+}
+
+// Falsy Value
+let firstname = "Rudra";
+
+if(firstname){
+    console.log(firstname);
+}else{
+    console.log("firstname is Kinda empty")
+}
+
+
+  //nested if else
+
+//   winning number 19
+
+//   19 your guess is right
+//   17 too low
+//   20 too high
+
+let winningNumber = 19;
+let userGuess = prompt("Guess a number");
+
+if (userGuess == winningNumber){
+    console.log("Your guess is right!!");
+}else{
+    // console.log("Your guess is wrong!!")
+    if(userGuess < winningNumber){
+        console.log("too low!!!");
+    }else{
+        console.log("too high !!!");
+    }
+}
+
+//else if
+
+let tempInDegree = 15;
+
+if(tempInDegree < 0){
+    console.log("extreamly cold outisde");
+}else if(tempInDegree < 16){
+    console.log("It is Cold outside");
+}else if(tempInDegree < 25){
+    console.log("wheather is okay");
+}else if (tempInDegree < 35){
+    console.log("lets go for swim");
+}else if (tempInDegree < 45){
+    console.log("turn on AC");
+}else{
+    console.log("too hot!!");
 }
---
+++ operator.js
@@ -0,0 +1,40 @@
+// ternary Operator / condtional operator
+// let age2 = 8;
+// let drink;
+
+// if (age2>=5){
+//     drink = "coffee"
+// }else{
+//     drink = "milk"
+// }
+// console.log(drink);
+//apply ternary Operator
+let age = 3;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
+
+// and or oerator 
+let firstName = "Rudra";
+let age3 = 22;
+
+// if(firstName[0]==="R"){
+//     console.log("your name starts with R")
+// }
+
+// if (age > 18){
+//     console.log("you are above 18");
+// }
+
+//apply and  operator
+// if(firstName[0]=== "R" && age>18){
+//     console.log("Name starts with H and above 18");
+// }else{
+//     console.log("inside else");
+// }
+
+//apply or operator
+if(firstName[0]=== "R" || age>18){
+    console.log("Inside if");
+}else{
+    console.log("inside else");
+}

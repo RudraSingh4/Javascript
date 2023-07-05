ac1ffd313deffcbbdd7a3ede0c36f8f93f73e02a
Rudra <rishurudrasingh@gmail.com>
2023-07-05 20:57:28 +0530

    Update

---
+++ Assignment/1st.js
@@ -0,0 +1,13 @@
+let age = 18;
+let user = prompt("User Can give Vote");
+
+if (age >18){
+    console.log("Able to Voterudra7046!!");
+}else{
+    // console.log("Your guess is wrong!!")
+    if(userGuess < winningNumber){
+        console.log("too low!!!");
+    }else{
+        console.log("too high !!!");
+    }
+}
